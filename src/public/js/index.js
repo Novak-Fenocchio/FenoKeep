193ac85d98d50d4e520c812fceb685d766dc09c6
= <=>
2021-03-23 12:22:06 -0300

    open and close form edit work!

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -13,3 +13,17 @@ $('.close-form').on('click', function(){
     $('#form-add-note').hide();
     $('#form-add-false').show();
 });
+
+/* Form to edit note */
+let id = undefined;
+$('.note-main').on('click', function(){
+     id = this.id;
+    $('#edit-note'+this.id).show();
+})
+
+$('.close-form').on('click', function()
+{
+    $('#edit-note'+id).hide();
+})
+
+/* close form to edit */
